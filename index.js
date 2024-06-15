71edfee62fa03cfbc0eb078787e226d6e36330c3
Shukri Abdi <shukri751@gmail.com>
2024-06-15 18:05:51 +0300

    index.js modified

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+  return "something";
+}
+
+function subtract(a ,b){
+  return "something";
+
+}
+function multiply(a,b){
+  return "something";
+}
+function divide( a,b){
+  return "something";
+}
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide( a,b){
+  return a/b
+}
+function increment(n) {
+  return n + 1;
+}
+function decrement(n) {
+  return n -1;
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
